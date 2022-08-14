27eae2c4fbe014e61647a0130ddada5d9564cd50
Jorge A. Torres S <86933137+joaltosa@users.noreply.github.com>
2022-08-14 18:09:49 -0500

    Update periAreaTriangulo.py
    Se modificó la hipotenusa como variable

--- periAreaTriangulo.py
+++ periAreaTriangulo.py
@@ -2,4 +2,7 @@
 base = float ( input ( ' Escriba el balor de la base  ' ) )
 altura = float ( input ( ' Ingrese el valor de la altura  ' ) )
 
-print ( ' el perímetro es ' , base + altura + ( base ** 2 + altura ** 2) ** 0.5 , 'y su área es ' , base * altura ) 
+# cálculo de la hipotenusa ( diagonal )
+hipotenusa = ( base ** 2 + altura ** 2 ) ** ( 1 / 2 ) )
+
+print ( ' el perímetro es ' , base + altura + hipotenusa , 'y su área es ' , base * altura ) 
