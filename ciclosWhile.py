f39b88ab319f98ade69f2ae1cdacc8c57394f865
joaltosa <madatove@gmail.com>
2022-08-17 03:05:12 -0500

    Corrigiendo el discriminante de cuadratiroot

---
+++ ciclosWhile.py
@@ -0,0 +1,4 @@
+valor = 18
+while valor >= 0:
+    print ( valor  / 3 )
+    valor -= 3
--- cuadraticsRoots.py
+++ cuadraticsRoots.py
@@ -1,25 +1,24 @@
-# Cálculo de lastaices de la ecuación cuadrática
+# Cálculo de las taices de la ecuación cuadrática
 A = float (input ( ' Ingrese el valor de A  ' ) )
 B = float (input ( ' Ingrese el valor de B  ' ) )
 C = float (input ( ' Ingrese el valor de C  ' ) )
 
 # Cálculo del discriminante
 
-disc = ( B ** 2 - 4 * A * C) ** ( 1 / 2 )
+disc =  B ** 2 - 4 * A * C
 
 print ( )
 # Análisis discriminante
-'''
-if disc < 0:
+
+if disc > 0:
     print ( ' Tiene 2 raíces reales diferentes ' )
-if disc == 0:
+elif disc == 0:
     print (' Tiene dos raíces iguales ' )
 else:
     print ( ' Tiene dos raíces complejas diferentes' )
 print ( )
 # Cálculo de las raíces
-'''
-print ( ' x1 = ' ,   ( - B + disc ) / ( 2 * A )  ) 
-print ( )
-print ( ' x2 = ' ,   ( - B - disc ) / ( 2 * A )  )
+
+print ( ' x1 = ' ,   ( - B + disc ** ( 1 / 2 ) ) / ( 2 * A )  ) 
+print ( ' x2 = ' ,   ( - B - disc ** ( 1 / 2 ) ) / ( 2 * A )  )
 print ( )
